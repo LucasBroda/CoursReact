798d78ed8b99a4e6b6a292f14824613066de72e8
lucas_broda <lulubrod@gmail.com>
2025-05-28 10:58:24 +0100

    feat(ListCount.tsx): added a component ListGroup that list a couple of Buttons

---
+++ front/src/components/ListCount/ListCount.tsx
@@ -0,0 +1,13 @@
+import Bouton from '@components/Bouton';
+
+const ListCount = () => <ul>
+  <li>
+    <Bouton />
+    <Bouton />
+    <Bouton />
+    <Bouton />
+    <Bouton />
+  </li>
+</ul>
+
+export default ListCount;
